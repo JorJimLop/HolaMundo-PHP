720e1254c7b83ae3774dc29f21a6c60a39db31ec
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-24 10:05:31 -0600

    Update routes.php
    MODIFICA

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
     
-    Route::get('/',function() { echo 'Hello, World!'; });
+    Route::get('/',function() { echo 'HOLA MUNDO!! Tarea 1, Jordan Jiménez López!'; });
     
     Route::dispatch();
 ?>
